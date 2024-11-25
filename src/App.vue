018b0fea103592594c512a1f98e9ac2ea2b553c0
Denis Popov <denvas.88@gmail.com>
2024-11-25 10:24:39 +0500

    feat: add form

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,32 @@
 </script>
 
 <template>
-
-
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <h1 class="text-center my-3">Учет товаров</h1>
+      <div class="mb-3">
+        <label for="name" class="form-label">Название</label>
+        <input type="text" class="form-control" id="name">
+      </div>
+      <div class="mb-3">
+        <label for="date" class="form-label">Дата добавления</label>
+        <input type="date" class="form-control" id="date">
+      </div>
+      <div class="mb-3">
+        <label for="count" class="form-label">Количество</label>
+        <input type="number" class="form-control" id="count">
+      </div>
+      <div class="mb-3">
+        <label for="price" class="form-label">Цена</label>
+        <input type="number" class="form-control" id="price">
+      </div>
+      <div>
+        <button type="button" class="btn btn-outline-success">Добавить</button>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 
